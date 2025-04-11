d2e8c03a95340c0a5d175897eba1711bf409d6f4
Tyler Melan <tymelan@gmail.com>
2025-04-10 19:20:21 -0700

    Store api_key in an environment variable instead of credentials for easy testing

--- app/services/blockchain_api_client.rb
+++ app/services/blockchain_api_client.rb
@@ -1,7 +1,7 @@
 require "net/http"
 
 class BlockchainApiClient
-  TRANSACTIONS_URI = URI("https://4816b0d3-d97d-47c4-a02c-298a5081c0f9.mock.pstmn.io/near/transactions?api_key=#{Rails.application.credentials.blockchain_api_key}")
+  TRANSACTIONS_URI = URI("https://4816b0d3-d97d-47c4-a02c-298a5081c0f9.mock.pstmn.io/near/transactions?api_key=#{ENV["BLOCKCHAIN_API_KEY"]}")
 
   def self.fetch_transactions
     response = Net::HTTP.get(TRANSACTIONS_URI)
